4f2c18ff26909780645b91008d50ef07a5a08a55
adriatrist4n <adriantristan287@gmail.com>
2023-12-11 23:47:59 +0100

    Cambios: - Se añaden un label para pedir al usurio la cantidad de alumnos - Se crea un array de notas - Se prepara para añadir las notas al array

--- app/src/main/java/com/example/pr401/MainActivity.kt
+++ app/src/main/java/com/example/pr401/MainActivity.kt
@@ -5,21 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,6 +40,8 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pr401.ui.theme.Brown80
@@ -40,8 +55,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             PR401Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    main()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Main()
                 }
             }
         }
@@ -50,7 +68,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun main(modifier: Modifier = Modifier) {
+fun Main(modifier: Modifier = Modifier) {
     // Caja que contiene toda la pantalla
     Box(
         modifier = modifier
@@ -109,10 +127,114 @@ fun main(modifier: Modifier = Modifier) {
                         .fillMaxWidth()
                         .align(Alignment.Center)
                 ) {
+                    Row {
+                        Column(
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                        ) {
+                            var numeroTexto by remember { mutableStateOf(TextFieldValue("")) }
+                            var numeroArray: Int by remember { mutableStateOf(0) }
+                            var alumnosArray by remember { mutableStateOf<IntArray?>(null) }
+                            var notasTexto by remember { mutableStateOf(TextFieldValue("")) }
 
+                            var botonPresionado by remember { mutableStateOf(false) }
+
+
+                            // OutlinedTextField para ingresar el número de alumnos
+                            if (!botonPresionado) {
+                                OutlinedTextField(
+                                    value = numeroTexto,
+                                    onValueChange = {
+                                        // Manejar el cambio de texto y convertirlo a Int
+                                        numeroTexto = it
+                                        numeroArray = it.text.toIntOrNull() ?: 0
+                                    },
+                                    label = { Text(text = "Ingresa el numero de alumnos del curso:") },
+                                    keyboardOptions = KeyboardOptions.Default.copy(
+                                        keyboardType = KeyboardType.Number
+                                    ),
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(bottom = 16.dp)
+                                )
+                            }
+
+                            // Botón para realizar alguna acción con el número
+                            if (!botonPresionado) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.End
+                                ) {
+                                    Button(
+                                        onClick = {
+                                            // Crear un array con el tamaño indicado
+                                            alumnosArray = IntArray(numeroArray)
+                                            botonPresionado = true
+                                        }
+                                    ) {
+                                        Icon(Icons.Default.Send, contentDescription = null)
+                                        Spacer(modifier = Modifier.width(4.dp))
+                                        Text("Enviar")
+                                    }
+                                }
+                            }
+
+                            if (botonPresionado && alumnosArray != null) {
+                                OutlinedTextField(
+                                    value = notasTexto,
+                                    onValueChange = {
+                                        // Manejar el cambio de texto y convertirlo a Int
+                                        notasTexto = it
+                                    },
+                                    label = { Text(text = "Ingresa las notas de los alumnos:") },
+                                    keyboardOptions = KeyboardOptions.Default.copy(
+                                        keyboardType = KeyboardType.Number
+                                    ),
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(top = 16.dp)
+                                )
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.End
+                                ) {
+                                    Button(
+                                        onClick = {
+                                            // Verificar que la nota no esté vacía
+                                            if (notasTexto.text.isNotBlank()) {
+                                                // Obtener el índice actual en el que se está insertando la nota
+                                                val indice = alumnosArray!!.indexOfFirst { it == 0 }
+                                                if (indice != -1) {
+                                                    // Insertar la nota en el array
+                                                    alumnosArray!![indice] = notasTexto.text.toInt()
+                                                    // Limpiar el campo de notas
+                                                    notasTexto = TextFieldValue("")
+                                                    // Realizar la acción deseada con las notas (en este ejemplo, imprimir el array)
+                                                    println("Notas ingresadas: ${alumnosArray!!.contentToString()}")
+                                                } else {
+                                                    // Mostrar un mensaje si el array está lleno
+                                                    println("El array de alumnos ya está lleno")
+                                                }
+                                            }
+                                        },
+                                        modifier = Modifier
+                                            .padding(top = 8.dp)
+                                    ) {
+                                        Text("Insertar Nota")
+                                    }
+                                    Text(
+                                        text = "Notas: ${alumnosArray?.contentToString()}",
+                                        modifier = Modifier.padding(top = 8.dp)
+                                    )
+                                }
+                            }
+
+                        }
+                    }
                 }
-        }
-
+            }
         }
     }
 }
